55960705fc8767b667f799db1e5491352d7f6c84
m1dn1ghtblue <kotyrov.1310@gmail.com>
2023-01-28 00:16:05 +0100

    fix layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,18 @@ module.exports = {
         current: 'currentColor',
         'black': '#1E1E24',
         'white': '#FFFFFF',
-        'grey': '#CCCCCC',
+        'grey': '#EEEEEE',
         'dark-grey': '#999999',
         'blue-green': '#16BAC5',
         'orange': '#FB5012',
       },
       fontFamily: {
       },
+      extend: {
+        gridTemplateColumns: {
+          'layout': '256px 1fr',
+        }
+      }
   },
   plugins: [],
 }
